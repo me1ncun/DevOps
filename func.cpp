12fe5955105c050e123e24f41f732b02b7cb5541
me1ncun <denis09112004@gmail.com>
2024-09-30 17:20:50 +0300

    Hotfix: FuncA returns sum of first 3 elements

--- func.cpp
+++ func.cpp
@@ -2,5 +2,14 @@
 #include <cmath>
 
 double FuncClass::FuncA(){
-	return 0;
+	double result = 1;
+	double x = 0.5;
+	double term = x/2;
+	
+	for(int i = 1; i < 3; ++i){
+	result += term;
+	term *= -x * (2* i - 1)/(2 * i);
+	}
+	
+	return result;
 }
